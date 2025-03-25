108c3185d0f39001e4d3eab2c91e1f9a49adb79f
EmmanuelCollante <gaming.max305.stream>
2025-03-25 15:21:11 -0500

    Ahora borra los archivos que no esten en origen

--- Logica.py
+++ Logica.py
@@ -10,7 +10,14 @@ root.withdraw()
 class UsbReader():
     def __init__(self):
         pass
-    
+    def borrar(self,origen,destino):
+        for archivo in os.listdir(destino):
+            ruta_origen = os.path.join(origen, archivo)
+            ruta_destino = os.path.join(destino, archivo)
+            if os.path.isfile(ruta_destino) and not os.path.exists(ruta_origen):
+                os.remove(ruta_destino)
+                print(f'Archivo eliminado de destino: {archivo}')
+
     def copiar(self,origen,destino):
         for archivo in os.listdir(origen):
             ruta_origen = os.path.join(origen, archivo)
@@ -18,12 +25,6 @@ class UsbReader():
             if os.path.isfile(ruta_origen) and not os.path.exists(ruta_destino):
                 shutil.copy(ruta_origen, ruta_destino)
                 print(f'Archivo copiado a destino: {archivo}')
-        for archivo in os.listdir(destino):
-            ruta_origen = os.path.join(origen, archivo)
-            ruta_destino = os.path.join(destino, archivo)
-            if os.path.isfile(ruta_destino) and not os.path.exists(ruta_origen):
-                shutil.copy(ruta_destino, ruta_origen)
-                print(f'Archivo copiado a origen: {archivo}')
 
     def consultar(self):
         nombreCarpeta = filedialog.askdirectory(title='Selecciona la carpeta de la USB')
@@ -33,11 +34,14 @@ class UsbReader():
             comparador = filedialog.askdirectory(title='Selecciona la carpeta donde va a copiar')
             if comparador:
                 print('existe')
+                self.borrar(nombreCarpeta, comparador)
                 self.copiar(nombreCarpeta, comparador)
             else:
                 print('no existe')       
         else:
             print('no existe')
+
+
 
 
 Usb = UsbReader()
